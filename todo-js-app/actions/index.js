b8d604058e62ade848cf456c3cf5298f658f22aa
venkateshwar <venkateshwar1996@gmail.com>
2026-01-22 13:12:32 +0530

    cleanup

--- todo-js-app/actions/index.js
+++
@@ -1,7 +0,0 @@
-import { addTodo } from './addTodo.js';
-import { completeTodo } from './completeTodo.js';
-import { deleteTodo } from './deleteTodo.js';
-
-const actions = [addTodo, completeTodo, deleteTodo];
-
-export { actions };
--- todo-js-app/runtime/history.js
+++
@@ -1,5 +0,0 @@
-// Event log
-
-const history = [];
-
-export { history };
