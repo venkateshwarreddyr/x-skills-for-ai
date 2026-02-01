91fc243634aea68c21a3b8753d519cba2b79acf5
venkateshwar <venkateshwar1996@gmail.com>
2026-02-01 16:01:56 +0530

    svelte updated code

---
+++ examples/svelte/svelte-counter/src/Counter.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { useXSkill } from '@x-skills-for-ai/svelte'
+  import { writable } from 'svelte/store'
+
+  let count = writable(0)
+
+  useXSkill({
+    id: 'increment',
+    description: 'Increase counter',
+    handler: async () => {
+      count.update(c => c + 1)
+    }
+  })
+
+  useXSkill({
+    id: 'decrement',
+    description: 'Decrease counter',
+    handler: async () => {
+      count.update(c => c - 1)
+    }
+  })
+
+  function inc() {
+    count.update(c => c + 1)
+  }
+
+  function dec() {
+    count.update(c => c - 1)
+  }
+</script>
+
+<h2 data-testid="count">Counter: {$count}</h2>
+<button on:click={inc}>Increment</button>
+<button on:click={dec}>Decrement</button>
